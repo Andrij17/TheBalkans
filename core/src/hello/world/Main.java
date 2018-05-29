417060544e42fb66a2f3203b250d36a494aa77d5
Unknown <mattttpatttt@gmail.com>
2018-05-29 10:39:25 -0400

    Auto stash before merge of "master" and "origin/master"

--- core/src/hello/world/Main.java
+++ core/src/hello/world/Main.java
@@ -2,7 +2,6 @@ package hello.world;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -10,39 +9,39 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img1;
-    Sprite spr1;
+	Texture img;
+    Sprite spr2;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img1 = new Texture("Mstanding.png");
-		spr1= new Sprite (img1);
-		spr1.setPosition(Gdx.graphics.getWidth()/2-spr1.getWidth()/2, Gdx.graphics.getHeight()/2-spr1.getHeight()/2);
-		spr1.setRotation(0f);
-		spr1.setScale(1f);
+		img = new Texture("Gomba1.png");
+		spr2= new Sprite (img);
+		spr2.setPosition(Gdx.graphics.getWidth()/2-spr1.getWidth()/2, Gdx.graphics.getHeight()/2-spr1.getHeight()/2);
+		spr2.setRotation(0f);
+		spr2.setScale(1f);
 	}
 
 	@Override
 	public void render () {
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            spr1.translateX(-2f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            spr1.translateX(2f);
-        }
-        Gdx.gl.glClearColor(0, 0, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	   if(spr2.input.isKeyPressed(Input.Keys.LEFT))
+	   	sprite.translateX(-1f);
+	   if(spr2.input.isKeyPressed(Input.Keys.RIGHT))
+	   sprite.translateX(1f);
+
+
+		Gdx.gl.glClearColor(0, 0, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(spr1, spr1.getX(), spr1.getY(), spr1.getWidth()/2,spr1.getHeight()/2, spr1.getWidth(), spr1.getHeight(),
                 spr1.getScaleX(), spr1.getScaleY(), spr1.getRotation());
 		batch.end();
 	}
 
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img1.dispose();
+		img.dispose();
 	}
 }
