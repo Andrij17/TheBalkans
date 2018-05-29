3ba798156193a6a38cf30a144309f5fd0fa1c081
Unknown <andrijaisakov@gmail.com>
2018-05-29 10:44:04 -0400

    Edits

--- core/src/hello/world/Main.java
+++ core/src/hello/world/Main.java
@@ -2,6 +2,7 @@ package hello.world;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -17,24 +18,24 @@ public class Main extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("Gomba1.png");
 		spr2= new Sprite (img);
-		spr2.setPosition(Gdx.graphics.getWidth()/2-spr1.getWidth()/2, Gdx.graphics.getHeight()/2-spr1.getHeight()/2);
+		spr2.setPosition(Gdx.graphics.getWidth()/2-spr2.getWidth()/2, Gdx.graphics.getHeight()/2-spr2.getHeight()/2);
 		spr2.setRotation(0f);
 		spr2.setScale(1f);
 	}
 
 	@Override
 	public void render () {
-	   if(spr2.input.isKeyPressed(Input.Keys.LEFT))
-	   	sprite.translateX(-1f);
-	   if(spr2.input.isKeyPressed(Input.Keys.RIGHT))
-	   sprite.translateX(1f);
+	   if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+	   	spr2.translateX(-1f);
+	   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+	   spr2.translateX(1f);
 
 
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(spr1, spr1.getX(), spr1.getY(), spr1.getWidth()/2,spr1.getHeight()/2, spr1.getWidth(), spr1.getHeight(),
-                spr1.getScaleX(), spr1.getScaleY(), spr1.getRotation());
+		batch.draw(spr2, spr2.getX(), spr2.getY(), spr2.getWidth()/2,spr2.getHeight()/2, spr2.getWidth(),
+                spr2.getHeight(),spr2.getScaleX(), spr2.getScaleY(), spr2.getRotation());
 		batch.end();
 	}
 
