191518ecf5c0e4cac8ea087500b50c8e3e11150c
Unknown <andrijaisakov@gmail.com>
2018-05-29 11:30:23 -0400

    Mario and goomba movement

--- core/src/hello/world/Main.java
+++ core/src/hello/world/Main.java
@@ -10,32 +10,48 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-    Sprite spr2;
+	Texture img1;
+	Texture img2;
+    Sprite Mstanding;
+    Sprite Goomba;
+    int i;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("MStanding.png");
-		spr2= new Sprite (img);
-		spr2.setPosition(Gdx.graphics.getWidth()/2-spr2.getWidth()/2, Gdx.graphics.getHeight()/2-spr2.getHeight()/2);
-		spr2.setRotation(0f);
-		spr2.setScale(1f);
+		img1 = new Texture("MStanding.png");
+        img2 = new Texture("Goomba_edited-1.png");
+        Mstanding= new Sprite (img1);
+		Goomba= new Sprite (img2);
+
+        Mstanding.setPosition(Gdx.graphics.getWidth()/2-Mstanding.getWidth()/2, Gdx.graphics.getHeight()/2-Mstanding.getHeight()/2);
+        Mstanding.setScale(1f);
+        Goomba.setPosition(Gdx.graphics.getWidth()/2-Mstanding.getWidth()/2,-100);
+        Goomba.setScale(0.5f);
 	}
 
 	@Override
 	public void render () {
+	    //Mario keyboard control (left & right)
 	   if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
-	   	spr2.translateX(-1f);
+           Mstanding.translateX(-1f);
 	   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-	   spr2.translateX(1f);
+           Mstanding.translateX(1f);
+
+        //Goomba sliding across
+            Goomba.translateX(-1f);
 
 
+	   //background
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(spr2, spr2.getX(), spr2.getY(), spr2.getWidth()/2,spr2.getHeight()/2, spr2.getWidth(),
-                spr2.getHeight(),spr2.getScaleX(), spr2.getScaleY(), spr2.getRotation());
+		//draw Mario
+		batch.draw(Mstanding, Mstanding.getX(), Mstanding.getY(), Mstanding.getWidth()/2,Mstanding.getHeight()/2,
+                Mstanding.getWidth(), Mstanding.getHeight(),Mstanding.getScaleX(), Mstanding.getScaleY(), Mstanding.getRotation());
+		//draw Goomba
+        batch.draw(Goomba, Goomba.getX(), Goomba.getY(), Goomba.getWidth()/2,Goomba.getHeight()/2,
+                Goomba.getWidth(), Goomba.getHeight(),Goomba.getScaleX(), Goomba.getScaleY(), Goomba.getRotation());
 		batch.end();
 	}
 
@@ -43,6 +59,6 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
 	}
 }
