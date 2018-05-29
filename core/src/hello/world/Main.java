deb9fab4a7b6648163bd247aa7731694b88ae7c3
Unknown <andrijaisakov@gmail.com>
2018-05-29 10:36:31 -0400

    Moving Mario

--- core/src/hello/world/Main.java
+++ core/src/hello/world/Main.java
@@ -2,6 +2,7 @@ package hello.world;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -9,23 +10,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img1;
     Sprite spr1;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("seventeen.png");
-		spr1= new Sprite (img);
+		img1 = new Texture("Mstanding.png");
+		spr1= new Sprite (img1);
 		spr1.setPosition(Gdx.graphics.getWidth()/2-spr1.getWidth()/2, Gdx.graphics.getHeight()/2-spr1.getHeight()/2);
 		spr1.setRotation(0f);
-		spr1.setScale(0.5f);
+		spr1.setScale(1f);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            spr1.translateX(-2f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            spr1.translateX(2f);
+        }
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(spr1, spr1.getX(), spr1.getY(), spr1.getWidth()/2,spr1.getHeight()/2, spr1.getWidth(), spr1.getHeight(),
                 spr1.getScaleX(), spr1.getScaleY(), spr1.getRotation());
@@ -36,6 +43,6 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
 	}
 }
