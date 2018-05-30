d1a130f6025465f944bff1aa93a0d7b502ba16f6
Unknown <mattttpatttt@gmail.com>
2018-05-30 10:15:46 -0400

    Sprite edit

--- core/src/hello/world/Main.java
+++ core/src/hello/world/Main.java
@@ -12,34 +12,34 @@ public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img1;
 	Texture img2;
-    Sprite Mstanding;
-    Sprite Goomba;
+    Sprite sprMstanding;
+    Sprite sprsprGoomba;
     int i;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img1 = new Texture("MStanding.png");
-        img2 = new Texture("Goomba_edited-1.png");
-        Mstanding= new Sprite (img1);
-		Goomba= new Sprite (img2);
+        img2 = new Texture("sprGoomba_edited-1.png");
+       sprMstanding= new Sprite (img1);
+		sprGoomba= new Sprite (img2);
 
-        Mstanding.setPosition(Gdx.graphics.getWidth()/2-Mstanding.getWidth()/2, Gdx.graphics.getHeight()/2-Mstanding.getHeight()/2);
-        Mstanding.setScale(1f);
-        Goomba.setPosition(Gdx.graphics.getWidth()/2-Mstanding.getWidth()/2,-100);
-        Goomba.setScale(0.5f);
+        sprMstanding.setPosition(Gdx.graphics.getWidth()/2-sprMstanding.getWidth()/2, Gdx.graphics.getHeight()/2-sprMstanding.getHeight()/2);
+        sprMstanding.setScale(1f);
+        sprsprGoomba.setPosition(Gdx.graphics.getWidth()/2-sprMstanding.getWidth()/2,-100);
+        sprsprGoomba.setScale(0.5f);
 	}
 
 	@Override
 	public void render () {
 	    //Mario keyboard control (left & right)
 	   if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
-           Mstanding.translateX(-1f);
+           sprMstanding.translateX(-1f);
 	   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-           Mstanding.translateX(1f);
+           sprMstanding.translateX(1f);
 
-        //Goomba sliding across
-            Goomba.translateX(-1f);
+        //sprGoomba sliding across
+            sprsprGoomba.translateX(-1f);
 
 
 	   //background
@@ -47,11 +47,11 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		//draw Mario
-		batch.draw(Mstanding, Mstanding.getX(), Mstanding.getY(), Mstanding.getWidth()/2,Mstanding.getHeight()/2,
-                Mstanding.getWidth(), Mstanding.getHeight(),Mstanding.getScaleX(), Mstanding.getScaleY(), Mstanding.getRotation());
-		//draw Goomba
-        batch.draw(Goomba, Goomba.getX(), Goomba.getY(), Goomba.getWidth()/2,Goomba.getHeight()/2,
-                Goomba.getWidth(), Goomba.getHeight(),Goomba.getScaleX(), Goomba.getScaleY(), Goomba.getRotation());
+		batch.draw(Mstanding,sprMstanding.getX(),sprMstanding.getY(),sprMstanding.getWidth()/2,Mstanding.getHeight()/2,
+               sprMstanding.getWidth(),sprMstanding.getHeight(),Mstanding.getScaleX(),sprMstanding.getScaleY(),sprMstanding.getRotation());
+		//draw sprGoomba
+        batch.draw(sprGoomba, sprGoomba.getX(), sprGoomba.getY(), sprGoomba.getWidth()/2,sprGoomba.getHeight()/2,
+                sprGoomba.getWidth(), sprGoomba.getHeight(),sprGoomba.getScaleX(), sprGoomba.getScaleY(), sprGoomba.getRotation());
 		batch.end();
 	}
 
